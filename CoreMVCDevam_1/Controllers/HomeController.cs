cace644ae110c4323af3a87af02009444542ab87
cagri.yolyapar <cagri.yolyapar@gmail.com>
2024-02-27 14:58:38 +0300

    Add project files.

---
+++ CoreMVCDevam_1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreMVCDevam_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CarpimTablosu()
+        {
+            return View();
+        }
+
+
+
+        /*
+         1x1 = 1
+         1x2 = 2
+         ..
+        .. 
+        ..
+         1x10 = 10
+         --------------------------
+        2 x 1 = 2
+        2x2 = 4
+        ...
+        2x10 = 20
+        -----------------------------
+         
+         
+         
+         
+         */
+    }
+}
